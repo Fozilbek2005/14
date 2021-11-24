34674e75449a75371ee22e333211fbf39152618b
TechLine <FOZILBEK20051127@gmail.com>
2021-11-24 19:47:58 +0600

    14 darss

---
+++ src/com/company/Bank.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Bank {
+    private String passwork;
+    private int amount;
+
+    public String getPasswork() {
+        return passwork;
+    }
+
+    public void setPasswork(String passwork) {
+        this.passwork = passwork;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
